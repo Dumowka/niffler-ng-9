1ff67c6ebafed2ccc65ee3079a738d88324c7d05
dzhirnov <sirdumowka@mail.ru>
2025-06-25 19:21:21 +0700

    Task 3.2: Выполнение домашнего задания

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/FriendsWebTest.java
@@ -0,0 +1,65 @@
+package guru.qa.niffler.test;
+
+import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.extension.BrowserExtension;
+import guru.qa.niffler.jupiter.extension.UsersQueueExtension;
+import guru.qa.niffler.jupiter.extension.UsersQueueExtension.StaticUser;
+import guru.qa.niffler.jupiter.extension.UsersQueueExtension.UserType;
+import guru.qa.niffler.jupiter.extension.UsersQueueExtension.UserType.Type;
+import guru.qa.niffler.page.LoginPage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static guru.qa.niffler.jupiter.extension.UsersQueueExtension.UserType.Type.WITH_FRIEND;
+
+@ExtendWith(BrowserExtension.class)
+public class FriendsWebTest {
+
+    private static final Config CFG = Config.getInstance();
+
+    @Test
+    @ExtendWith(UsersQueueExtension.class)
+    void friendShouldBePresentInFriendsTable(@UserType(WITH_FRIEND) StaticUser user) {
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .fillLoginPage(user.username(), user.password())
+                .submit()
+                .checkThatPageLoaded()
+                .goToUserFriendsPage()
+                .checkFriends(user.friend());
+    }
+
+    @Test
+    @ExtendWith(UsersQueueExtension.class)
+    void friendsTableShouldBeEmptyForNewUser(@UserType(Type.EMPTY) StaticUser user) {
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .fillLoginPage(user.username(), user.password())
+                .submit()
+                .checkThatPageLoaded()
+                .goToUserFriendsPage()
+                .checkNoFriend();
+    }
+
+    @Test
+    @ExtendWith(UsersQueueExtension.class)
+    void incomeInvitationBePresentInFriendsTable(@UserType(Type.WITH_INCOME_REQUEST) StaticUser user) {
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .fillLoginPage(user.username(), user.password())
+                .submit()
+                .checkThatPageLoaded()
+                .goToUserFriendsPage()
+                .checkIncomeInvitation(user.income());
+    }
+
+    @Test
+    @ExtendWith(UsersQueueExtension.class)
+    void outcomeInvitationBePresentInAllPeopleTable(@UserType(Type.WITH_OUTCOME_REQUEST) StaticUser user) {
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .fillLoginPage(user.username(), user.password())
+                .submit()
+                .checkThatPageLoaded()
+                .goToUserFriendsPage()
+                .clickOnAllPeopleTable()
+                .checkOutcomeInvitation(user.outcome());
+    }
+}
