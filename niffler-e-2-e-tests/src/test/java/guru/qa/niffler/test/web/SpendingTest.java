c9689ae7b4e881fba50343840b2501d77218139e
dzhirnov <sirdumowka@mail.ru>
2025-08-23 12:01:24 +0700

    Sync fork: Allure Lifecycle, add service logs to report, first screenshot test.

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingTest.java
@@ -2,6 +2,7 @@ package guru.qa.niffler.test.web;
 
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.annotation.ScreenShotTest;
 import guru.qa.niffler.jupiter.annotation.Spending;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
@@ -10,9 +11,16 @@ import guru.qa.niffler.model.spend.SpendJson;
 import guru.qa.niffler.model.userdata.UserJson;
 import guru.qa.niffler.page.LoginPage;
 import guru.qa.niffler.utils.RandomDataUtils;
+import guru.qa.niffler.utils.ScreenDiffResult;
 import org.junit.jupiter.api.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Date;
+
+import static com.codeborne.selenide.Selenide.$;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @WebTest
 public class SpendingTest {
@@ -59,5 +67,26 @@ public class SpendingTest {
                 .setDescription(newDescription)
                 .saveCreatedSpending()
                 .checkThatTableContainsSpending(newDescription);
+    }
+
+    @User(
+            spendings = @Spending(
+                    amount = 79990.00,
+                    description = "Advanced 9 поток!",
+                    category = "Обучение"
+            )
+    )
+    @ScreenShotTest("img/expected-stat.png")
+    void checkStatComponentTest(UserJson user, BufferedImage expected) throws IOException {
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .fillLoginPage(user.username(), user.testData().password())
+                .submit()
+                .checkThatPageLoaded();
+
+        BufferedImage actual = ImageIO.read($("canvas[role='img']").screenshot());
+        assertFalse(new ScreenDiffResult(
+                expected,
+                actual
+        ));
     }
 }
