674511d3e2e4017c89d525d39b05e9c64b4109cb
dzhirnov <sirdumowka@mail.ru>
2025-09-04 19:52:24 +0700

    Task 11.1: Выполнение домашнего задания

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/AuthApi.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/AuthApi.java
@@ -1,6 +1,9 @@
 package guru.qa.niffler.api;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
@@ -19,5 +22,33 @@ public interface AuthApi {
             @Query("password") String password,
             @Query("passwordSubmit") String passwordSubmit,
             @Query("_csrf") String token
+    );
+
+    @GET("oauth2/authorize")
+    Call<Void> authorize(
+            @Query("response_type") String responseType,
+            @Query("client_id") String clientId,
+            @Query("scope") String scope,
+            @Query(value = "redirect_uri", encoded = true) String redirectUri,
+            @Query("code_challenge") String codeChallenge,
+            @Query("code_challenge_method") String codeChallengeMethod
+    );
+
+    @FormUrlEncoded
+    @POST("/login")
+    Call<Void> login(
+            @Field("username") String username,
+            @Field("password") String password,
+            @Field("_csrf") String token
+    );
+
+    @FormUrlEncoded
+    @POST("oauth2/token")
+    Call<JsonNode> token(
+            @Field("code") String code,
+            @Field(value = "redirect_uri", encoded = true) String redirectUri,
+            @Field("code_verifier") String codeVerifier,
+            @Field("grant_type") String grantType,
+            @Field("client_id") String clientId
     );
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/AuthApiClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/AuthApiClient.java
@@ -1,15 +1,19 @@
 package guru.qa.niffler.api;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import guru.qa.niffler.api.core.ThreadSafeCookieStore;
 import guru.qa.niffler.model.userdata.UserJson;
 import guru.qa.niffler.service.AuthClient;
 import guru.qa.niffler.service.RestClient;
 import org.apache.commons.lang3.time.StopWatch;
+import retrofit2.Response;
 
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ParametersAreNonnullByDefault
 public class AuthApiClient extends RestClient implements AuthClient {
@@ -52,5 +56,58 @@ public class AuthApiClient extends RestClient implements AuthClient {
             }
         }
         throw new AssertionError("Timed out waiting for register");
+    }
+
+    @Override
+    public void authorize(String codeChallenge) {
+        Response<Void> response;
+        try {
+            response = authApi.authorize(
+                    "code",
+                    "client",
+                    "openid",
+                    CFG.frontUrl() + "authorized",
+                    codeChallenge,
+                    "S256"
+
+            ).execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(200, response.code());
+    }
+
+    @Override
+    public String login(String username, String password) {
+        Response<Void> response;
+        try {
+            response = authApi.login(
+                    username,
+                    password,
+                    ThreadSafeCookieStore.INSTANCE.cookieValue("XSRF-TOKEN")
+            ).execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(200, response.code());
+        return response.raw().request().url().toString().split("\\?code=")[1];
+    }
+
+    @Override
+    public String token(String code, String codeVerifier) {
+        Response<JsonNode> response;
+        try {
+            response = authApi.token(
+                    code,
+                    CFG.frontUrl() + "authorized",
+                    codeVerifier,
+                    "authorization_code",
+                    "client"
+            ).execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(200, response.code());
+        return response.body().get("id_token").asText();
     }
 }
